08b248df0ff161c5e64fbeab12c180799da15bde
ankit <dev2@junctiontech.in>
2015-10-31 15:16:07 +0530

    test for api

---
+++ application/controllers/remoteapi.php
@@ -0,0 +1,50 @@
+<?php 
+
+/* Controller for login Functionality */
+class Remoteapi {
+	
+	function locationUpdate(){
+		$CONNECTION=mysqli_connect("localhost",'root','bitnami','appmanager');
+		if($CONNECTION)
+		{
+			$data=json_decode($_POST['employeeData']);
+			$imei=$data->employeeIMEI;//echo $imei;
+			foreach ($data->employeeLocationList as $list)
+				{
+					$result = "INSERT INTO tracking VALUES('".$imei."','".$list->employeeLocationDate."','".$list->employeeLocationTime."','".$list->employeeLocationLatitude."','".$list->employeeLocationLongitude."','".$list->employeeLocationProviderName."','".$list->employeeLocationBatteryLevel."')";
+					$sql=mysqli_query($CONNECTION,$result);
+				}
+				if($sql)
+				{
+					/*$data=array(
+							
+							'code'=>'200',
+							'result'=>'true',
+								);
+					print_r($data);die;*/
+					 echo 'true'; die;
+				}
+				else 
+				{
+					/*$data=array(
+					
+							'code'=>'400',
+							'result'=>'false',
+					);*/
+					echo 'false'; die;
+				}
+		}
+		else
+		{
+			echo 'connection error';
+		}
+	}
+	
+	function task_update()
+	{
+		// task update body hear
+	}
+	
+	
+}
+/* End of login controller */
