91b90386f33646df938f43aa72ca650f9a209f58
ankit <dev2@junctiontech.in>
2015-11-04 15:11:34 +0530

    test

--- js/common_function.js
+++ js/common_function.js
@@ -66,7 +66,7 @@ function check_dbname(dbname,id)
 		 return false;
 }
 
-/*function application_entry(value)
+function application_entry(value)
 {	
 	if(document.getElementById('chk').checked==true)
 		{     
@@ -90,5 +90,28 @@ function check_dbname(dbname,id)
 		}
 	
 	//alert(email);
-}*/
+}
 
+/*
+function CustomAlert(){
+    this.render = function(dialog){
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = winH+"px";
+        dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+        document.getElementById('dialogboxhead').innerHTML = "Acknowledge This Message";
+        document.getElementById('dialogboxbody').innerHTML = dialog;
+        document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
+    }
+	this.ok = function(){
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
+	}
+}
+var Alert = new CustomAlert();
+*/
