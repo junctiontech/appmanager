01419d31993a9e72eddd50ef609acd3ad216a00b
ankit <dev2@junctiontech.in>
2015-11-05 16:48:51 +0530

    test

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -17,12 +17,12 @@ class Admin_model extends CI_Model{
 	function org_list($data=false)
 	{
 		
-		if($data && $username['username']!=='admin')			// when organization admin login admin panel and create session in admin login part
+		if($data)			// when organization admin login admin panel and create session in admin login part
 		{ 
 			$qry=$this->db->query("select organizations.*,registered_application.* from organizations,registered_application where organizations.organization_id=$data && registered_application.organization_id=$data");
 			return $qry->result();
 		}
-		if($username['username']=='admin')
+		else
 		{
 			$qry=$this->db->query("select organizations.*,registered_application.* from organizations,registered_application where organizations.organization_id=registered_application.organization_id");
 			return $qry->result();
