8c84f4b9b73f4ba9f0acf16b3029081594c63b2c
ankit <dev2@junctiontech.in>
2015-11-06 17:43:22 +0530

    test

--- application/controllers/admin_panel.php
+++ application/controllers/admin_panel.php
@@ -36,7 +36,7 @@
  					'Username'=>$this->input->post('username'),
  					'Password'=>md5($this->input->post('password'))
  			);
- 			$query=$this->admin_model->verify_admin('organizations',$data);
+ 			$query=$this->admin_model->verify_admin('organizations',$data);print_r($query);die;
  			$qry=$this->admin_model->org_list($query[0]->organization_id);
  				if($qry){ 
 			 			$session_data=array(
