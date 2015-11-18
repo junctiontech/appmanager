a176e4083a6c634b79b4334959025a1b31a582c5
ankit <dev2@junctiontech.in>
2015-11-18 15:53:14 +0530

    test

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -77,9 +77,9 @@ class Remoteapi {
 		if($CONNECTION!=='')
 		{
 			$query= "select * from project";
-			$sql=mysqli_query($CONNECTION,$query);
-			$result=mysqli_num_rows($sql);print_r($result);die;
-			echo json_encode($result);die;
+			$sql=mysqli_query($CONNECTION,$query);//print_r($sql);die;
+			//$result=mysqli_num_rows($sql);print_r($result);die;
+			echo json_encode($sql);die;
 			if($sql)
 			{
 				$query= "select * from task";
