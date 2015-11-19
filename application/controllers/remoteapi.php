d007b4875d8604fe0ace5cf2d39270bbe8a94194
ankit <dev2@junctiontech.in>
2015-11-19 16:01:07 +0530

    test

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -100,7 +100,7 @@ class Remoteapi {
 							}
 						}
 						
-						$data_array=array_push($project_data,$task_data);
+						$data_array=array_merge($project_data,$task_data);
 						$result	=	array(
 								'project_list'		=>$data_array,
 								//'task_of_list'		=>	$task_data,
