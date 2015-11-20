714f584171829375243130f10cb922592c7c2499
ankit <dev2@junctiontech.in>
2015-11-20 12:19:07 +0530

    test for application

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -142,7 +142,7 @@ class Remoteapi {
 						}
 						$local_var=array('task_of_list'=>$task_data);
 						print_r($project_data[0]);die;
-						array_push($project_data,$local_var);
+						array_push($project_data,$local_var); 
 						
 						die;
 						/*for($i=0;$i<count($task_data);$i++)
