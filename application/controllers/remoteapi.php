453e2aa93974a9db18c84a9a87e7f736837fb143
ankit <dev2@junctiontech.in>
2015-11-20 17:54:36 +0530

    test json

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -163,9 +163,9 @@ class Remoteapi {
 				while($result_project=mysqli_fetch_assoc($sqls))
 				{
 					//$j=0;
-					$project_id[]=$result_project['project_id'];
-					$project_description[]= $result_project['project_description'];
-					$status[]= $result_project['status'];
+					$project_id=$result_project['project_id'];
+					$project_description= $result_project['project_description'];
+					$status= $result_project['status'];
 					
 					
 					
