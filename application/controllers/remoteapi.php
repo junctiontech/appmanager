35b95a0c727f0816f9f28b51b010559b8e0e6f92
ankit <dev2@junctiontech.in>
2015-11-20 11:56:51 +0530

    test

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -127,9 +127,10 @@ class Remoteapi {
 			{
 					while($result_project=mysqli_fetch_assoc($sqls))
 					{
+						$j='';
 						$project_data[]=$result_project;
-					//	echo $result_project['project_id'];die;
-						$query= "select * from task where project_id='".$result_project['project_id']."'";
+						echo $result_project[$i]['project_id'];die;
+						$query= "select * from task where project_id='".$result_project[$j]['project_id']."'";
 						$sql=mysqli_query($CONNECTION,$query);
 						$count=mysqli_num_rows($sql);
 						$task_data	=	array();
