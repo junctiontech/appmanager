95e53fc13c01bda0b1a6b021113964e22b524a27
ankit <dev2@junctiontech.in>
2015-11-20 17:50:30 +0530

    test json format for api

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -189,7 +189,7 @@ class Remoteapi {
 					//		'test'=>'demo',
 					//		'testing'=>'demoing',
 					//);
-					$project_list[]=array('id'=>$project_id,
+					$project_list[]=array('id'=>$project_id, 
 							'desc'=>$project_description,
 							'status'=>$status,
 							'task_of_list'=>$task_data,
