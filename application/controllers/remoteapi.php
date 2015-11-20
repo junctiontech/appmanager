1955f5a849b99483b8051c56b4ceb851e8a5b4f3
ankit <dev2@junctiontech.in>
2015-11-20 13:23:08 +0530

    test

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -11,7 +11,7 @@ class Remoteapi {
 			$imei=$data->employeeIMEI;
 			foreach($data->employeeLocationList as $list)
 				{
-					$result = "INSERT INTO tracking VALUES('".$imei."','".$list->employeeLocationDate."','".$list->employeeLoce."','".$list->employeeLocationTime."','".$list->employeeLocationLatitude."','".$list->employeeLocationLongitude."','".$list->employeeLocationProviderName."','".$list->employeeLocationBatteryLevel."')";
+					$result = "INSERT INTO tracking VALUES('".$imei."','".$list->employeeLocationDate."','".$list->employeeLocationTime."','".$list->employeeLocationLatitude."','".$list->employeeLocationLongitude."','".$list->employeeLocationProvidbberName."','".$list->employeeLocationBatteryLevel."')";
 					$sql=mysqli_query($CONNECTION,$result);
 				}
 				if($sql)
