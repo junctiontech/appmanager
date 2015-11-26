08caea664b85ed202710f3f45b299cb6a00f75fe
ankit <dev2@junctiontech.in>
2015-11-26 16:15:56 +0530

    echo for sms api

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -240,7 +240,7 @@ Class Login extends CI_Controller {
 						if($app_info[0]->status=='active')  // check status application active or not
 						{
 							$explode=explode("@",$value->username);
-							if($explode>1) 
+							if($explode>1)  
 							{ echo '@';
 								$data=array(
 												'url'=>$url_name,
