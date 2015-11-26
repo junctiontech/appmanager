883c1d7f9a07ef0b4d239939e0f5f30345d4887f
ankit <dev2@junctiontech.in>
2015-11-26 16:32:49 +0530

    test

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -239,8 +239,8 @@ Class Login extends CI_Controller {
 					{
 						if($app_info[0]->status=='active')  // check status application active or not
 						{
-							$explode=explode("@",$value->username); echo count($explode);die;
-							if(count($explode)>2)  
+							$explode=explode("@",$value->username); 
+							if(count($explode)== 2)  
 							{ 
 								$data=array(
 												'url'=>$url_name,
