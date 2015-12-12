0e87f087d48c0a77af31fe4294d9a71bd60ce55b
ankit <dev2@junctiontech.in>
2015-12-12 15:45:15 +0530

    test

--- application/controllers/AppmanagerGateway.php
+++ application/controllers/AppmanagerGateway.php
@@ -4,20 +4,20 @@ class AppmanagerGateway extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->helper('url');
-		$this->load->model('AppmanagerGateway_model');
+		//$this->load->helper('url');
+		$this->load->model('appmanagergateway_model');
 	}
 	function CheckAuthonticate($Filter=false)
 	{  
 		echo $Filter;die;
 		$url= $_SERVER['HTTP_REFERER']; echo $url;die;
-		$CheckDatabaseName=$this->data['CheckDatabaseName']=$this->AppmanagerGateway_model->GetSingleData('registered_application',array('db_name'=>$Filter));//print_r($CheckDatabaseName);die;
+		$CheckDatabaseName=$this->data['CheckDatabaseName']=$this->appmanagergateway_model->GetSingleData('registered_application',array('db_name'=>$Filter));//print_r($CheckDatabaseName);die;
 		if($CheckDatabaseName)
 		{
-			$CheckOrganizationStatus=$this->data['CheckOrganizationStatus']=$this->AppmanagerGateway_model->GetSingleData('organizations',array('organization_id'=>$CheckDatabaseName[0]->organization_id,'status'=>'active'));//print_r($CheckOrganizationStatus);die;
+			$CheckOrganizationStatus=$this->data['CheckOrganizationStatus']=$this->appmanagergateway_model->GetSingleData('organizations',array('organization_id'=>$CheckDatabaseName[0]->organization_id,'status'=>'active'));//print_r($CheckOrganizationStatus);die;
 			if($CheckOrganizationStatus)
 			{
-				$CheckApplicationStatus=$this->data['CheckApplicationStatus']=$this->AppmanagerGateway_model->GetSingleData('registered_application',array('registration_id'=>$CheckDatabaseName[0]->registration_id,'status'=>'active'));//print_r($CheckApplicationStatus);die;
+				$CheckApplicationStatus=$this->data['CheckApplicationStatus']=$this->appmanagergateway_model->GetSingleData('registered_application',array('registration_id'=>$CheckDatabaseName[0]->registration_id,'status'=>'active'));//print_r($CheckApplicationStatus);die;
 				if($CheckApplicationStatus)
 				{
 					$result= 'success';
