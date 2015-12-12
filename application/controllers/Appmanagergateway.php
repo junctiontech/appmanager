edfb1ce5e3fd4d87b0ba3035dea672ef6bb3f69c
ankit <dev2@junctiontech.in>
2015-12-12 16:41:30 +0530

    test

--- application/controllers/Appmanagergateway.php
+++ application/controllers/Appmanagergateway.php
@@ -14,7 +14,7 @@ class Appmanagergateway extends CI_Controller
 	function CheckAuthonticate($Filter=false)
 	{  
 		//echo $Filter;die;
-		//$url= $_SERVER['HTTP_REFERER']; echo $url;die;
+		$url= $_SERVER['HTTP_REFERER']; echo $url;die;
 		$CheckDatabaseName=$this->data['CheckDatabaseName']=$this->appmanagergateway_model->GetSingleData('registered_application',array('db_name'=>$Filter));//print_r($CheckDatabaseName);die;
 		if($CheckDatabaseName)
 		{
