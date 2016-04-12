2ec860eb5aabba65c5690ed0f8b2af8eee88072f
nabeela <nabans1811@gmail.com>
2016-04-12 18:14:21 +0530

    hi

--- application/views/registration_application.php
+++ application/views/registration_application.php
@@ -1,6 +1,6 @@
 <!-- add organization page added by palak on 20 th june -->
 <!-- add organization body starts -->
-<body style="background-color: #7d3669" >
+<body style="background-color: #E58121" >
 <!--  <div class="page-loading-overlay">
 	<div class="loader-2"></div>
 </div>-->
