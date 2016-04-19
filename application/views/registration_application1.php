79927404746619c4b3e5d923925f7454ef8466b0
nabeela <nabans1811@gmail.com>
2016-04-19 12:08:53 +0530

    change in code

--- application/views/registration_application1.php
+++ application/views/registration_application1.php
@@ -107,7 +107,7 @@
 			<div class="form-group">
 				<label class="col-sm-2 control-label" for="field-1">Organization Name</label>
 				<div class="col-sm-10">
-					<input type="text" class="form-control" name="organization_name" id="org" placeholder="Organization Name" onchange="check_organization(this.value,this.id)" required >
+					<input type="text" class="form-control" name="organization_name" id="org" onkeyup="myfunction()" placeholder="Organization Name" onchange="check_organization(this.value,this.id)" required >
 				</div>
 			</div>
 			<div class="form-group">
