82600bf41a242720dc0a0323464644e98b7e960b
nabeela <nabans1811@gmail.com>
2016-04-19 16:17:47 +0530

    hi

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -642,6 +642,7 @@ Class Login extends CI_Controller {
 	function school($schoolname=false)
 	{
 		$school_detail=$this->data['schooldetail']=$this->login_model->schooldetail($schoolname);
+		print_r($school_detail);die;
 		$this->parser->parse('include/header',$this->data);
 		$this->load->view('school',$this->data);
 		$this->parser->parse('include/footer',$this->data);
