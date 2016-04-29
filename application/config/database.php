6c9d54adb8f5c346f1f4899ed9f76f9c2334ecf3
Junction Ankit <Junction Ankit@JunctionAnkit>
2016-04-29 13:30:07 +0530

    asdsadf

--- application/config/database.php
+++ application/config/database.php
@@ -48,6 +48,10 @@
    $CI =& get_instance();
     $CI->load->library('session'); //if it's not autoloaded in your CI setup
 	$database_name=$CI->session->userdata('db_name'); 
+	if($CI->session->userdata('db_name')=='')
+	{
+		$dbname='appmanager'
+	}
 	//$CI->session->unset_userdata($database_name);
 	//$CI->session->sess_destroy();die;
 if($_SERVER['HTTP_HOST']=="localhost"){
